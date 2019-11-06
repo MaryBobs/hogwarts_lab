38e8ab5f6a1d5b3f4694b1befd9dda91abec4db2
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-11-06 19:45:37 +0000

    added foreign key

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,6 @@
 require_relative('../models/student.rb')
 require_relative('../models/house.rb')
 
-
-student1 = Student.new (
-  {
-    "first_name" => "Harry",
-    "last_name" => "Potter",
-    "house" => "Gryffindor",
-    "age" => 10
-  }
-)
-
-student2 = Student.new(
-  {
-    "first_name" => "Draco",
-    "last_name" => "Malfoy",
-    "house" => "Slytherin",
-    "age" => 12
-  }
-)
-
-student1.save()
-student2.save()
-
 house1 = House.new({"name" => "Gryffindor", "url" => "/public/images/Gryffindor"})
 house2 = House.new({"name" => "Ravenclaw", "url" => "/public/images/Ravenclaw"})
 house3 = House.new({"name" => "Slytherin", "url" => "/public/images/Slytherin"})
@@ -32,3 +10,24 @@ house1.save()
 house2.save()
 house3.save()
 house4.save()
+
+student1 = Student.new (
+  {
+    "first_name" => "Harry",
+    "last_name" => "Potter",
+    "house_id" => 1,
+    "age" => 10
+  }
+)
+
+student2 = Student.new(
+  {
+    "first_name" => "Draco",
+    "last_name" => "Malfoy",
+    "house_id" => 3,
+    "age" => 12
+  }
+)
+
+student1.save()
+student2.save()
--- models/student.rb
+++ models/student.rb
@@ -2,13 +2,13 @@ require_relative('../db/sql_runner.rb')
 
 class Student
 
-attr_reader :id, :first_name, :last_name, :house, :age
+attr_reader :id, :first_name, :last_name, :house_id, :age
 
 def initialize(options)
   @id = options['id'].to_i if options['id']
   @first_name = options['first_name']
   @last_name = options['last_name']
-  @house = options['house']
+  @house_id = options['house_id']
   @age = options['age'].to_i
 end
 
@@ -17,7 +17,7 @@ def save()
     (
   first_name,
   last_name,
-  house,
+  house_id,
   age
   )
   VALUES
@@ -25,7 +25,7 @@ def save()
     $1, $2, $3, $4
     )
     RETURNING id"
-    values = [@first_name, @last_name, @house, @age]
+    values = [@first_name, @last_name, @house_id, @age]
     student = SqlRunner.run(sql, values)
     @id = student.first()['id'].to_i
 end
